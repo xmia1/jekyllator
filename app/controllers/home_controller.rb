b6a377a0ce3bbdcc7ee912cea21960f15384f335
manuraj17 <manu.elayath@gmail.com>
2016-06-04 07:36:20 +0000

    update to index for selecting blog repo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,29 @@
 class HomeController < ApplicationController
+
+  def index
+    @user = User.find_by(id: session[:user_id])
+
+    if @user.blog_repo == nil
+      url = @user.repo_url
+      puts url
+      uri = URI.parse(url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      #http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      request = Net::HTTP::Get.new(uri.request_uri)
+      #user = ENV['FUNCTIONAL_ID']
+      #  pw = ENV['FUNCTIONAL_PW']
+      #request.basic_auth(user, pw)
+      response = http.request(request)
+      @repos_hash = JSON.parse(response.body)
+      #redirect to select blog
+    else
+      #redirect to list repo files
+    end
+
+  end
+
+  def create
+    @blog_repo = params[:repo_id]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resources :home
   root 'home#index'
   get '/auth/:provider/callback', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
